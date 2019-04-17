bce88c7ecca06d6e8ae08d666d50579101395ad1
doDomoGu <kaka_71936410@qq.com>
2019-04-17 17:48:04 +0800

    vue-ls

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from "vue"
 import App from "./App.vue"
 import store from "./store"
 
+/* mint-ui */
 import Mint from "mint-ui"
 import "mint-ui/lib/style.css"
-
 Vue.use(Mint)
+
+/* LocalStorage 本地储存 */
+import Storage from "vue-ls"
+import { STORAGE_OPTIONS } from "@/utils/constant"
+Vue.use(Storage, STORAGE_OPTIONS)
 
 Vue.config.productionTip = false
 
