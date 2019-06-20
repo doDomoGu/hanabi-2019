b9dfe87b77e0719549db7d3219cad2690e229a6a
doDomoGu <kaka_71936410@qq.com>
2019-06-20 15:24:32 +0800

    全局ctx 样式参数

--- src/App.vue
+++ src/App.vue
@@ -67,17 +67,17 @@ export default {
 body,
 html {
   margin: 0;
-  background: #99c9db;
+  background: $background;
 }
 
 .ctx {
   position: absolute;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   width: $width;
-  height: ($width / 10 * 16);
-  // top: 50%;
-  // margin-top: (-99vw / 2 / 9 * 16);
-  // left: 50%;
-  // margin-left: (-99vw / 2);
+  height: $height;
+  top: 50%;
+  margin-top: -$height / 2;
+  left: 50%;
+  margin-left: -$width / 2;
 }
 </style>
