9c7a38349548f9dc94bb09de93b73d44ce6a14a6
doDomoGu <kaka_71936410@qq.com>
2019-07-04 16:02:17 +0800

    draw config RoomList

--- src/utils/canvas/config/roomList.js
+++ src/utils/canvas/config/roomList.js
@@ -7,34 +7,44 @@ import bgImg from "@/assets/hanabi_bg2.jpg"
 
 let _ = {}
 
-const windowPaddingTop = 8 * vh // 视窗顶部留白距离
-const windowPaddingRight = 4 * vw // 视窗右边留白距离
+const windowPaddingTop = 8 * vh //视窗顶部留白距离
+const windowPaddingRight = 4 * vw //视窗右边留白距离
 
-const itemSpacing = 2 * vh // 房间项间距
+const itemTextFont = px2Rem(20) + "px " + fontFamily //房间项内文字字体
+const itemTextColorActived = "#ffffff" //房间项选中（激活/touchstart）状态文字颜色
+const itemTextColorUnactived = "#333333" //房间项未选中（未激活/touchend/初始状态）状态文字颜色
+const itemBgColorActived = "#6699cc" //房间项选中状态（激活/touchstart）文字颜色
+const itemBgColorUnactived = "#eeeeee" //房间项未选中（未激活/touchend/初始状态）状态背景颜色
 
-const itemWidth = 30 * vw
-const itemHeight = 6 * vh
+const itemSpacing = 2 * vh //房间项间距
+
+const itemW = 30 * vw //房间项宽度
+const itemH = 6 * vh //房间项高度
+
+const itemTitleOffsetX = 1 * vw //房间项标题距离左边偏移量
+const itemLockOffsetX = 15 * vw //房间项标题左偏移量
+const itemPlayerNumOffsetX = 20 * vw //房间项标题左偏移量
 
 _.item = {}
 
 _.item.text = {}
-_.item.text.font = px2Rem(20) + "px " + fontFamily
+_.item.text.font = itemTextFont
 _.item.text.color = {}
-_.item.text.color.actived = "#ffffff"
-_.item.text.color.unactived = "#333333"
+_.item.text.color.actived = itemTextColorActived
+_.item.text.color.unactived = itemTextColorUnactived
 _.item.bgColor = {}
-_.item.bgColor.actived = "#6699cc"
-_.item.bgColor.unactived = "#eeeeee"
+_.item.bgColor.actived = itemBgColorActived
+_.item.bgColor.unactived = itemBgColorUnactived
 
 _.item.rect = {}
-_.item.rect.x = 100 * vw - itemWidth - windowPaddingRight
+_.item.rect.x = 100 * vw - itemW - windowPaddingRight
 _.item.rect.y = NavbarConfig.rect.h + windowPaddingTop
-_.item.rect.w = itemWidth
-_.item.rect.h = itemHeight
+_.item.rect.w = itemW
+_.item.rect.h = itemH
 
-_.item.titleX = _.item.rect.x + 1 * vw
-_.item.lockX = _.item.rect.x + _.item.rect.w - 8 * vw
-_.item.playerCountX = _.item.rect.x + _.item.rect.w - 6 * vw
+_.item.titleX = _.item.rect.x + itemTitleOffsetX
+_.item.lockX = _.item.rect.x + itemLockOffsetX
+_.item.playerCountX = _.item.rect.x + itemPlayerNumOffsetX
 
 _.item.spacing = itemSpacing
 
