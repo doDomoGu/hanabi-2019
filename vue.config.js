d2dc032712dbfd865b80f5f80a950a33ebfcd1a8
doDomoGu <kaka_71936410@qq.com>
2019-08-23 15:37:53 +0800

    gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     config.plugins.push(
       new CompressionWebpackPlugin({
         // 正在匹配需要压缩的文件后缀
-        test: /.(js|css|svg|woff|ttf|json|html)$/,
+        test: /.(jpg|js|css|svg|woff|ttf|json|html)$/,
         // 大于10kb的会压缩
         threshold: 10240
         // 其余配置查看compression-webpack-plugin
